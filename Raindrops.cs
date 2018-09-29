c3ee2a553bd326ba2230df5576747f1228165882
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-09-30 03:42:53 +0530

    Implemented Logic

--- Raindrops.cs
+++ Raindrops.cs
@@ -2,8 +2,33 @@ using System;
 
 public static class Raindrops
 {
+    /// <summary>
+    /// If the number has 3 as a factor, output 'Pling'. 
+    /// If the number has 5 as a factor, output 'Plang'.
+    /// If the number has 7 as a factor, output 'Plong'.
+    /// Else return the number
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        String Output = String.Empty;
+        if(number % 3 == 0)
+        {
+            Output = Output + "Pling";
+        }
+        if(number % 5 == 0)
+        {
+            Output = Output + "Plang";
+        }
+        if (number % 7 == 0)
+        {
+            Output = Output + "Plong";
+        }
+        if (String.IsNullOrEmpty(Output))
+        {
+            Output = number.ToString();
+        }
+        return Output;
     }
 }
