92a34dda6c5afeba4485978dcefbacd8491b28cd
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-01 14:22:24 +0530

    adapted to style guide.

--- Raindrops.cs
+++ Raindrops.cs
@@ -2,33 +2,21 @@ using System;
 
 public static class Raindrops
 {
-    /// <summary>
-    /// If the number has 3 as a factor, output 'Pling'. 
-    /// If the number has 5 as a factor, output 'Plang'.
-    /// If the number has 7 as a factor, output 'Plong'.
-    /// Else return the number
-    /// </summary>
-    /// <param name="number"></param>
-    /// <returns></returns>
     public static string Convert(int number)
     {
-        String Output = String.Empty;
+        string output = string.Empty;
         if(number % 3 == 0)
-        {
-            Output = Output + "Pling";
-        }
-        if(number % 5 == 0)
-        {
-            Output = Output + "Plang";
-        }
+            output += "Pling";
+        
+        if(number % 5 == 0)      
+            output += "Plang";
+        
         if (number % 7 == 0)
-        {
-            Output = Output + "Plong";
-        }
-        if (String.IsNullOrEmpty(Output))
-        {
-            Output = number.ToString();
-        }
-        return Output;
+            output += "Plong";
+    
+        if (string.IsNullOrEmpty(output))        
+            output = number.ToString();
+        
+        return output;
     }
 }
